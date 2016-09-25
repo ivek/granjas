4aae34f0204bd4bf03e30ce3605c98d40f105451
ivek <ivekdelfin@gmail.com>
2016-09-25 22:41:03 +0000

    inicio

---
+++ apps/granjasalmoral/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GranjasalmoralConfig(AppConfig):
+    name = 'granjasalmoral'
---
+++ apps/granjasalmoral/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class InfoPersonal(models.Model):
+    id_personal=models.CharField(max_length=30)
+    nombre=models.CharField(max_length=50)
+    apellidos=models.CharField(max_length=200)
+    curp=models.CharField(max_length=50)
+    rfc=models.CharField(max_length=30)
+    genero=models.CharField(max_length=20)
+    edo_civil=models.CharField(max_length=20)
+    direccion=models.TextField()
+    telefono=models.CharField(max_length=20)
+    email=models.EmailField()
+    facebook=models.CharField(max_length=100)
+    otra_red=models.CharField(max_length=100)
+    puesto=models.CharField(max_length=200)
+    hra_entrada=models.DateTimeField()
+    hra_salida=models.DateTimeField()
+    fech_engreso=models.DateField()
