e34c1d7013a60a4595d7f5fe5659805b66995188
ivek <ivekdelfin@gmail.com>
2016-09-25 22:57:47 +0000

    modelo informacion del personal

--- apps/granjasalmoral/models.py
+++ apps/granjasalmoral/models.py
@@ -7,6 +7,7 @@ class InfoPersonal(models.Model):
     apellidos=models.CharField(max_length=200)
     curp=models.CharField(max_length=50)
     rfc=models.CharField(max_length=30)
+    edad=models.IntegerField(default=0)
     genero=models.CharField(max_length=20)
     edo_civil=models.CharField(max_length=20)
     direccion=models.TextField()
@@ -18,3 +19,4 @@ class InfoPersonal(models.Model):
     hra_entrada=models.DateTimeField()
     hra_salida=models.DateTimeField()
     fech_engreso=models.DateField()
+    img = models.FileField(upload_to='uploads/',default="")
